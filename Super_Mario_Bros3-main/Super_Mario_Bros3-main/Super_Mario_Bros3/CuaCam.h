86befb2f2afaf74e9790e04b4f089754e45e0fd5
kimquanghuy99 <18520411@gm.uit.edu.vn>
2022-04-04 23:17:36 +0700

    Revert "update enemy class"
    This reverts commit 16565fe4558d855adc16c02b2a1ee282e0295e03.

--- Super_Mario_Bros3-main/Super_Mario_Bros3-main/Super_Mario_Bros3/CuaCam.h
+++ Super_Mario_Bros3-main/Super_Mario_Bros3-main/Super_Mario_Bros3/CuaCam.h
@@ -1,26 +1,5 @@
 #pragma once
-#pragma once
-#include "GameObject.h"
-
-#define CUACAM_WALKING_SPEED 0.05f;
-
-#define CUACAM_BBOX_WIDTH 16
-#define CUACAM_BBOX_HEIGHT 15
-#define CUACAM_BBOX_HEIGHT_DIE 9
-
-#define CUACAM_STATE_WALKING 100
-#define CUACAM_STATE_DIE 200
-
-#define CUACAM_ANI_WALKING 0
-#define CUACAM_ANI_DIE 1
-
-class CCuaCam : public CGameObject
+class CuaCam
 {
-	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
-	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
-	virtual void Render();
-
-public:
-	CCuaCam();
-	virtual void SetState(int state);
 };
+
--- Super_Mario_Bros3-main/Super_Mario_Bros3-main/Super_Mario_Bros3/Soldier.h
+++ Super_Mario_Bros3-main/Super_Mario_Bros3-main/Super_Mario_Bros3/Soldier.h
@@ -1,26 +1,5 @@
 #pragma once
-#pragma once
-#include "GameObject.h"
-
-#define SOLDIER_WALKING_SPEED 0.05f;
-
-#define SOLDIER_BBOX_WIDTH 16
-#define SOLDIER_BBOX_HEIGHT 15
-#define SOLDIER_BBOX_HEIGHT_DIE 9
-
-#define SOLDIER_STATE_WALKING 100
-#define SOLDIER_STATE_DIE 200
-
-#define SOLDIER_ANI_WALKING 0
-#define SOLDIER_ANI_DIE 1
-
-class CSoldier : public CGameObject
+class Soldier
 {
-	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
-	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
-	virtual void Render();
-
-public:
-	CSoldier();
-	virtual void SetState(int state);
 };
+
