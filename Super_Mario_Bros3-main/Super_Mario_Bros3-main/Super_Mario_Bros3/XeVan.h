3ec821b8b7c3bad1cee413695e9888cb80ca03ce
Khanh2110 <18520402@gm.uit.edu.vn>
2022-04-04 21:59:12 +0700

    update xavan class

--- Super_Mario_Bros3-main/Super_Mario_Bros3-main/Super_Mario_Bros3/XeVan.h
+++ Super_Mario_Bros3-main/Super_Mario_Bros3-main/Super_Mario_Bros3/XeVan.h
@@ -1,17 +1,17 @@
 #pragma once
 #include "GameObject.h"
 
-#define GOOMBA_WALKING_SPEED 0.05f;
+#define XEVAN_WALKING_SPEED 0.05f;
 
-#define GOOMBA_BBOX_WIDTH 16
-#define GOOMBA_BBOX_HEIGHT 15
-#define GOOMBA_BBOX_HEIGHT_DIE 9
+#define XEVAN_BBOX_WIDTH 16
+#define XEVAN_BBOX_HEIGHT 15
+#define XEVAN_BBOX_HEIGHT_DIE 9
 
-#define GOOMBA_STATE_WALKING 100
-#define GOOMBA_STATE_DIE 200
+#define XEVAN_STATE_WALKING 100
+#define XEVAN_STATE_DIE 200
 
-#define GOOMBA_ANI_WALKING 0
-#define GOOMBA_ANI_DIE 1
+#define XEVAN_ANI_WALKING 0
+#define XEVAN_ANI_DIE 1
 
 class CXeVan : public CGameObject
 {
